f121944c4cba01c13c957a1d20423ae506e6f585
elnarmen <101528347+elnarmen@users.noreply.github.com>
2022-09-19 22:45:36 +0300

    Publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,7 +7,7 @@ def main():
     load_dotenv()
     token = os.getenv('TELEGRAM_TOKEN')
     bot = telegram.Bot(token=token)
-    print(bot.get_me())
+    bot.send_message(chat_id='@kosmo_photo_api', text="I'm sorry Dave I'm afraid I can't do that.")
 
 
 if __name__ == '__main__':
