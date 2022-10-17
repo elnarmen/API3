ee08c00f8c606970aab30c9fb451d2d5309849f1
elnarmen <101528347+elnarmen@users.noreply.github.com>
2022-10-17 18:12:22 +0300

    Add an env variable for the count of photos

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -5,24 +5,25 @@ from spase_images_api import download_picture
 from dotenv import load_dotenv
 
 
-def fetch_nasa_apod(api_key):
+def fetch_nasa_apod(api_key, token):
     url = f'https://api.nasa.gov/planetary/apod'
     payload = {
-        'count': 20,
+        'count': token,
         'api_key': api_key
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
     for i, item in enumerate(response.json()):
-        extention = get_extension(item["url"])
+        extention = get_extension(item['url'])
         if extention:
             download_picture(item['url'], f'images/nasa_apod_{i}{extention}')
 
 
 def main():
     load_dotenv()
+    count_of_images = os.getenv('COUNT_OF_IMAGES')
     api_key = os.getenv('API_KEY')
-    fetch_nasa_apod(api_key)
+    fetch_nasa_apod(api_key, count_of_images)
 
 
 if __name__ == '__main__':
