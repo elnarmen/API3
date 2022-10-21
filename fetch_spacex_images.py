5203b5c67a14e7bf55b989dfafd9c4f89d0d700b
elnarmen <101528347+elnarmen@users.noreply.github.com>
2022-10-21 12:26:21 +0300

    Reduce nesting of 'fetch_spacex_last_launch' function

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,21 +5,27 @@ from spase_images_api import download_picture
 from spase_images_api import get_extension
 
 
-def fetch_spacex_last_launch(id):
+def get_urls(id):
     url = f'https://api.spacexdata.com/v5/launches/{id}'
     response = requests.get(url)
     response.raise_for_status()
     urls = response.json()['links']['flickr']['original']
-    if not urls:
-        response = requests.get('https://api.spacexdata.com/v5/launches')
-        response.raise_for_status()
-        i = -2
-        while not response.json()[i]['links']['flickr']['original']:
-            i -= 1
-        urls = response.json()[i]['links']['flickr']['original']
-        print(f'''
-        В выбранном запуске отсутствуют фото. Будут загружены
-        фотографии запуска с 'id': {response.json()[i]['id']}''')
+    if urls:
+        return urls
+    response = requests.get('https://api.spacexdata.com/v5/launches')
+    response.raise_for_status()
+    i = -2
+    while not response.json()[i]['links']['flickr']['original']:
+        i -= 1
+    urls = response.json()[i]['links']['flickr']['original']
+    print(f'''
+    В выбранном запуске отсутствуют фото. Будут загружены
+    фотографии запуска с 'id': {response.json()[i]['id']}''')
+    return urls
+
+
+def fetch_spacex_last_launch(id):
+    urls = get_urls(id)
     for i, url in enumerate(urls):
         download_picture(str(url), f'images/spacex_{i}{get_extension(url)}')
 
